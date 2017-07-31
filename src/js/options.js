2c2349fafcb7bb5896b330868f7cfca9a0d4b8a4
Christian Varisco <vari.chri1993@gmail.com>
2017-08-01 00:10:28 +0200

    Add options logic

--- src/js/options.js
+++ src/js/options.js
@@ -1,22 +1,75 @@
-import Storage from "./storage";
+import { KEYS, saveOnStore, getFromStore } from "./storage";
 
-const GIR = {
-  init() {
-    $(window).on("load", () => {});
-  }
-};
-
-$("#submit").on("click", () => {
-  chrome.storage.sync.set(
+/**
+ * Clear the storage and exec a callback
+ */
+function clearStorage(callback) {
+  saveOnStore(
     {
-      apikey: $("#apikey").val(),
-      projectName: $("#projectName").val(),
-      gitlabUrl: $("#gitlabUrl").val()
+      [KEYS.API_KEY]: null,
+      [KEYS.PROJECT_NAME]: null,
+      [KEYS.GITLAB_URL]: null
     },
-    function() {
-      console.log("asd");
+    callback
+  );
+}
+
+/**
+ * Get options from storage and fill the form
+ */
+function initForm() {
+  getFromStore(
+    {
+      [KEYS.API_KEY]: null,
+      [KEYS.PROJECT_NAME]: null,
+      [KEYS.GITLAB_URL]: null
+    },
+    ({ API_KEY, PROJECT_NAME, GITLAB_URL }) => {
+      $("#apiKey").val(API_KEY);
+      $("#projectName").val(PROJECT_NAME);
+      $("#gitlabURL").val(GITLAB_URL);
     }
   );
-});
+}
 
-GIR.init();
+/**
+ * Save options parameters 
+ */
+function attacchSaveHandler() {
+  $("#options-form").on("submit", e => {
+    e.preventDefault();
+    const apiKey = $("#apiKey").val();
+    const projectName = $("#projectName").val();
+    const gitlabURL = $("#gitlabURL").val();
+
+    saveOnStore({
+      [KEYS.API_KEY]: apiKey,
+      [KEYS.PROJECT_NAME]: projectName,
+      [KEYS.GITLAB_URL]: gitlabURL
+    });
+  });
+}
+
+/**
+ * Clear form and storage from saved options
+ */
+function attachClearHandler() {
+  $("#delete-button").on("click", e => {
+    e.preventDefault();
+    clearStorage(() => {
+      $("#apiKey").val(null);
+      $("#projectName").val(null);
+      $("#gitlabURL").val(null);
+    });
+  });
+}
+
+function init() {
+  $(window).on("load", () => {
+    initForm();
+    attacchSaveHandler();
+    attachClearHandler();
+  });
+}
+
+init();
--- src/js/storage.js
+++ src/js/storage.js
@@ -1,4 +1,15 @@
-export default {
+const KEYS = {
   API_KEY: "API_KEY",
-  PROJECT_SELECTED: "PROJECT_SELECTED"
+  PROJECT_NAME: "PROJECT_NAME",
+  GITLAB_URL: "GITLAB_URL"
 };
+
+function saveOnStore(obj, callback = () => {}) {
+  chrome.storage.sync.set(obj, callback);
+}
+
+function getFromStore(obj, callback = () => {}) {
+  chrome.storage.sync.get(obj, callback);
+}
+
+export { KEYS, saveOnStore, getFromStore };
